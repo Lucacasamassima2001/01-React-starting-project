ae4cb4fd1db0d4053f4896c0b662a54014c4c63c
Luca Casamassima <casamassimaluca723@gmail.com>
2023-12-22 18:01:28 +0100

    First commit react starting prj.

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
+import { useState } from "react";
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleClick(selectedButton) {
+    // console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+  }
+
+  // if condition
+
+  // let tabContent = <p>Please select a topic.</p>;
+
+  // if (selectedTopic) {
+  //   tabContent = (
+  //     <div id="tab-content">
+  //       <h3>{EXAMPLES[selectedTopic].title}</h3>
+  //       <p>{EXAMPLES[selectedTopic].description}</p>
+  //       <pre>
+  //         <code>{EXAMPLES[selectedTopic].code}</code>
+  //       </pre>
+  //     </div>
+  //   );
+  // }
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleClick("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleClick("state")}>State</TabButton>
+          </menu>
+
+          {/* Ternary condition  */}
+
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
+
+          {/* if condition  */}
+          {/* {tabContent} */}
+
+          {/* Binary condition  */}
+
+          {/* {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )} */}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
